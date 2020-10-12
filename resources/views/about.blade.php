053862d238b2a063f5cadcc911bbf94f3884a1dc
Ary-Asyari <aryasyaricoc1@gmail.com>
2020-10-11 19:16:58 -0500

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.main')
+
+@section('title', 'About Laravel CRUD')
+    
+@section('container')
+    
+   <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h1 class="mt-3">About Laravel CRUD {{ $nama }}</h1>
+            </div>
+        </div>
+    </div>
+
+@endsection
